4b79c2246d7ce74ea24192e93f6812eeacfc1ebf
yeison34 <jeisonperenguez1998@gmail.com>
2023-08-02 20:04:21 -0500

    Primer Commit

---
+++ Trabajos de Grado UDENAR/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Trabajos_de_Grado_UDENAR
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Trabajos de Grado UDENAR/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Trabajos_de_Grado_UDENAR.MvcApplication" Language="C#" %>
